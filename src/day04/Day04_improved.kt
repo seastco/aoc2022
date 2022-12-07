b54d14695e543cbd1eb369fa480d318f21d0ca5a
seastco <seastcott@outlook.com>
2022-12-07 01:52:21 -0800

    day4 improved

---
+++ src/day04/Day04_improved.kt
@@ -0,0 +1,33 @@
+package day04
+
+import readLines
+
+private fun String.asIntRange(): IntRange =
+    substringBefore("-").toInt()..substringAfter("-").toInt()
+
+private fun String.asRanges(): Pair<IntRange,IntRange> =
+    substringBefore(",").asIntRange() to substringAfter(",").asIntRange()
+
+
+private infix fun IntRange.fullyOverlaps(other: IntRange): Boolean =
+    first <= other.first && last >= other.last
+
+private infix fun IntRange.overlaps(other: IntRange): Boolean =
+    first <= other.last && other.first <= last
+
+fun part1(input: List<String>): Int {
+    val ranges: List<Pair<IntRange, IntRange>> = input.map { it.asRanges() }
+    return ranges.count { it.first fullyOverlaps it.second || it.second fullyOverlaps it.first }
+}
+
+fun part2(input: List<String>): Int {
+    val ranges: List<Pair<IntRange, IntRange>> = input.map { it.asRanges() }
+    return ranges.count { it.first overlaps it.second }
+}
+
+fun main() {
+    println(part1(readLines("day04/test")))
+    println(part1(readLines("day04/input")))
+    println(part2(readLines("day04/test")))
+    println(part2(readLines("day04/input")))
+}
