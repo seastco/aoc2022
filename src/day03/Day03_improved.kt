daeabb65515f2e5c2654b460c07062899a7f3d09
seastco <seastcott@outlook.com>
2022-12-06 21:17:29 -0800

    improve day3

---
+++ src/day03/Day03_improved.kt
@@ -0,0 +1,36 @@
+package day03
+
+import readLines
+
+private fun Char.priority(): Int =
+    when (this) {
+        in 'a'..'z' -> (this - 'a') + 1
+        in 'A'..'Z' -> (this - 'A') + 27
+        else -> throw IllegalArgumentException("Letter not in range: $this")
+    }
+
+private fun List<String>.sharedItem(): Char =
+    map { it.toSet() } // convert List<String> to List<Set<Char>>
+        .reduce { left, right -> left intersect right } // intersect each set from left to right
+        .first() // return the shared Char
+
+private fun String.sharedItem(): Char =
+    listOf(
+        substring(0..length / 2),
+        substring(length / 2)
+    ).sharedItem()
+
+fun part1(input: List<String>): Int {
+    return input.sumOf { it.sharedItem().priority() }
+}
+
+fun part2(input: List<String>): Int {
+    return input.chunked(3).sumOf { it.sharedItem().priority() }
+}
+
+fun main() {
+    println(part1(readLines("day03/test")))
+    println(part1(readLines("day03/input")))
+    println(part2(readLines("day03/test")))
+    println(part2(readLines("day03/input")))
+}
--- src/day03/Day03_part2.kt
+++ src/day03/Day03_part2.kt
@@ -8,9 +8,9 @@ fun main() {
         var total = 0
         val groups = rucksacks.chunked(3)
         for (group in groups) {
-            val set1 = group.get(0).toSet()
-            val set2 = group.get(1).toSet()
-            val set3 = group.get(2).toSet()
+            val set1 = group[0].toSet()
+            val set2 = group[1].toSet()
+            val set3 = group[2].toSet()
             val sharedChar = set1.intersect(set2).intersect(set3).distinct()[0]
             val priority = if (sharedChar.isLowerCase()) sharedChar.code - 96 else sharedChar.code - 38
             total += priority
