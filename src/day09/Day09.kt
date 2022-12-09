0c51b0fb252a1897434d8f047925a6da95b99de7
seastco <seastcott@outlook.com>
2022-12-09 00:09:17 -0800

    day9 refactored

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -1,6 +1,7 @@
 package day09
 
 import readLines
+import kotlin.math.abs
 
 data class Knot(var x: Int, var y: Int)
 
@@ -11,49 +12,9 @@ enum class Direction(val x: Int, val y: Int) {
     D(0, -1)
 }
 
-fun part1(input: List<String>): Int {
-    val set = HashSet<Pair<Int, Int>>()
-    set.add(Pair(0, 0))
-    val head = Knot(0, 0)
-    val tail = Knot(0, 0)
-
-    for (move in input) {
-        val direction = Direction.valueOf(move.substringBefore(" "))
-        var distance = move.substringAfter(" ").toInt()
-
-        while (distance > 0) {
-            head.x += direction.x
-            head.y += direction.y
-
-            val xDistance = head.x - tail.x
-            val yDistance = head.y - tail.y
-
-            if (yDistance == -2) {
-                tail.x = head.x
-                tail.y = head.y + 1
-            } else if (yDistance == 2) {
-                tail.x = head.x
-                tail.y = head.y - 1
-            } else if (xDistance == -2) {
-                tail.x = head.x + 1
-                tail.y = head.y
-            } else if (xDistance == 2) {
-                tail.x = head.x - 1
-                tail.y = head.y
-            }
-
-            set.add(Pair(tail.x, tail.y))
-            distance -= 1
-        }
-    }
-
-    return set.size
-}
-
-fun part2(input: List<String>): Int {
+fun tailVisitedCount(input: List<String>, ropeLength: Int): Int {
     val rope = ArrayList<Knot>()
-    (1..10).map { rope.add(Knot(0, 0)) }
-
+    (1..ropeLength).map { rope.add(Knot(0, 0)) }
     val set = HashSet<Pair<Int, Int>>()
     set.add(Pair(0, 0))
 
@@ -65,51 +26,38 @@ fun part2(input: List<String>): Int {
             rope[0].x += direction.x
             rope[0].y += direction.y
 
-            for (i in 1..9) {
+            for (i in 1 until ropeLength) {
                 val knot1 = rope[i-1]
                 val knot2 = rope[i]
-
                 val xDistance = knot1.x - knot2.x
                 val yDistance = knot1.y - knot2.y
 
-                if (xDistance == 2 && yDistance == 2) {
-                    knot2.x += 1
-                    knot2.y += 1
-                } else if (xDistance == -2 && yDistance == -2) {
-                    knot2.x -= 1
-                    knot2.y -= 1
-                } else if (xDistance == -2 && yDistance == 2) {
-                    knot2.x -= 1
-                    knot2.y += 1
-                } else if (xDistance == 2 && yDistance == -2) {
-                    knot2.x += 1
-                    knot2.y -= 1
-                } else if (yDistance == -2) {
+                if (abs(xDistance) == 2 && abs(yDistance) == 2) {
+                    knot2.x += xDistance / 2
+                    knot2.y += yDistance / 2
+                } else if (abs(yDistance) == 2) {
                     knot2.x = knot1.x
-                    knot2.y = knot1.y + 1
-                } else if (yDistance == 2) {
-                    knot2.x = knot1.x
-                    knot2.y = knot1.y - 1
-                } else if (xDistance == -2) {
-                    knot2.x = knot1.x + 1
+                    knot2.y = knot1.y + (yDistance / 2) * -1
+                } else if (abs(xDistance) == 2) {
+                    knot2.x = knot1.x + (xDistance / 2) * -1
                     knot2.y = knot1.y
-                } else if (xDistance == 2) {
-                    knot2.x = knot1.x - 1
-                    knot2.y = knot1.y
-                } else {
-                    continue
-                }
-
-                if (i == 9) {
-                    set.add(Pair(knot2.x, knot2.y))
                 }
             }
 
+            set.add(Pair(rope[ropeLength-1].x, rope[ropeLength-1].y))
             distance -= 1
         }
     }
 
     return set.size
+}
+
+fun part1(input: List<String>): Int {
+    return tailVisitedCount(input, 2)
+}
+
+fun part2(input: List<String>): Int {
+    return tailVisitedCount(input, 10)
 }
 
 fun main() {
